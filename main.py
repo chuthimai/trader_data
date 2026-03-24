cd28938492168634f3cdb97d9e5ff729fda21ef0
maichu <chuthimai18245@gmail.com>
2026-03-24 22:32:21 +0700

    cập nhật danh sách mã

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def send_message(msg):
 
 def get_current_data(stock_code: str):
     # ngày hôm nay
-    END_DATE = datetime.today().strftime("%Y-%m-%d")
+    END_DATE = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
 
     # ngày trước hôm nay 150 ngày ~ 6 tháng
     START_DATE = (datetime.today() - timedelta(days=150)).strftime("%Y-%m-%d")
@@ -50,13 +50,13 @@ def format_stock(stock_code, data):
             command = "BUY"
 
     if command is None:
-        return None
+        return ""
 
     return f"{stock_code} | {low:.0f} | {high:.0f} | {rsi:.2f} | {choppy:.2f} | {command}"
 
 
 if __name__ == '__main__':
-    stock_codes = ["E1VFVN30", "FUEVFVND", "FUEMAV30", "FUESSV30", "FUEKIV30"]
+    stock_codes = ["E1VFVN30", "FUEVFVND", "FUESSV30"]
 
     results = []
     for stock_code in stock_codes:
